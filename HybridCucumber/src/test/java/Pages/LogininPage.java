c5567aa066d805b6b126bf70849690123aeb346d
sakthir <sakthir@NICDM193.nousinfo.com>
2022-02-02 17:10:09 +0530

    HybridCucumber from commit

--- HybridCucumber/src/test/java/Pages/LogininPage.java
+++ HybridCucumber/src/test/java/Pages/LogininPage.java
@@ -1,5 +1,33 @@
 package Pages;
 
-public class LogininPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class LogininPage {
+	WebDriver driver;
+	@FindBy(id="txtUsername")
+	WebElement username;
+	@FindBy(id="txtPassword")
+	WebElement password;
+	@FindBy(id="btnLogin")
+	WebElement login;
+	public LogininPage(WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver ,this);
+	}
+	public void login(String uname,String psword)
+	{
+		username.sendKeys(uname);
+		password.sendKeys(psword);
+	
+		
+	}
+	
+	public void submit() {
+		login.click();
+		
+	}
 }
--- HybridCucumber/src/test/java/TestCases/loginTestCase.java
+++ HybridCucumber/src/test/java/TestCases/loginTestCase.java
@@ -1,5 +1,35 @@
 package TestCases;
 
-public class loginTestCase {
+import java.io.IOException;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import BaseClass.LidraryClass;
+import Pages.LogininPage;
+import SeleniumResuableFunction.Seleniumutility;
+
+public class loginTestCase extends LidraryClass{
+Seleniumutility selenium;
+	LogininPage lp;
+	@BeforeMethod
+	public void openapp()
+	{
+		launchapp("chrome","https://opensource-demo.orangehrmlive.com/");
+	}
+	@Test
+	public void login()
+	{
+		 lp=new LogininPage(driver);
+		lp.login("Admin","admin123");
+	}
+	
+	@AfterMethod
+	public void teardown() throws IOException
+	{
+	 selenium=new Seleniumutility(driver);
+	 selenium.screenshot("C:\\Users\\sakthir\\git\\HybridCucumber\\HybridCucumber\\src\\test\\resources\\Screenshortslogin.png");
+	 closeapp();
+	}
 }
